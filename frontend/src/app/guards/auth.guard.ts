8102a4deffd5759dc32e578b534ded95b0bb7ac1
Suhoveev Dmitriy <dima.suhoveev@mail.ru>
2021-06-22 14:33:47 +0300

    bugfix

--- frontend/src/app/guards/auth.guard.ts
+++ frontend/src/app/guards/auth.guard.ts
@@ -9,7 +9,7 @@ export class AuthGuard implements CanActivate {
 
     constructor(private authService: AuthService, private router: Router) { }
 
-    canActivate(): any {
+    canActivate(): boolean {
     if (!this.authService.isLoggedIn()) {
         this.router.navigate(['/auth/authorization']);
     }
